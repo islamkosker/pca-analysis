292800968c2cb565995a8a5de2292c3cd2888d64
islamkosker <73235010+islamkosker@users.noreply.github.com>
2024-04-03 15:27:15 +0300

    latest

--- model/main.py
+++ model/main.py
@@ -6,13 +6,16 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 
-def get_image_paths(dir_path, category, sample_size=5):
-    all_images = os.listdir(os.path.join(dir_path, category))
-    selected_files = random.sample(all_images, sample_size)
-    selected_image_paths = [
-        os.path.join(dir_path, category, img) for img in selected_files
+def get_image_paths(dir_path, category):
+    category_path = os.path.join(dir_path, category)
+    all_images = [
+        f
+        for f in os.listdir(category_path)
+        if os.path.isfile(os.path.join(category_path, f))
     ]
-    return selected_image_paths, selected_files
+    selected_image_paths = [os.path.join(category_path, img) for img in all_images]
+
+    return selected_image_paths
 
 
 def load_images(image_paths):
@@ -48,27 +51,31 @@ def top_50_eigenvalues(eigenvalues):
     plt.show()
 
 
-def top_10_eigenfaces(eigenvectors, prefix="eigenface"):
-    top_10_eigenvectors = eigenvectors[:10]  
-    eigenfaces = top_10_eigenvectors.reshape((10, 64, 64))  # Reshape to 64x64 for each eigenface
-    
-    fig, axes = plt.subplots(2, 5, figsize=(15, 6))  # 2 rows, 5 columns of subplots for the top 10 eigenfaces
-    fig.suptitle('Top 10 Eigenfaces', fontsize=16)
-    
+def top_10_eigenfaces(eigenvectors, prefix="eigenface", n_eigenfaces=10):
+    top_10_eigenvectors = eigenvectors[:n_eigenfaces]
+    eigenfaces = top_10_eigenvectors.reshape(
+        (10, 64, 64)
+    )  # Reshape to 64x64 for each eigenface
+
+    fig, axes = plt.subplots(
+        2, 5, figsize=(15, 6)
+    )  # 2 rows, 5 columns of subplots for the top 10 eigenfaces
+    fig.suptitle("Top 10 Eigenfaces", fontsize=16)
+
     for i, eigenface in enumerate(eigenfaces):
-        ax = axes[i // 5, i % 5]  
-        ax.imshow(eigenface, cmap='gray')
+        ax = axes[i // 5, i % 5]
+        ax.imshow(eigenface, cmap="gray")
         ax.set_title(f"{prefix} {i+1}")
-        ax.axis('off')  # Hide axis
-    
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])  
-    plt.savefig(f"{prefix}_combined.png")  
-    plt.show()  
-    plt.close() 
+        ax.axis("off")  # Hide axis
+
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.savefig(f"{prefix}_combined.png")
+    plt.show()
+    plt.close()
 
 
 def project_to_new_space(data, eigenvectors):
-    return np.dot(data - np.mean(data, axis=0), eigenvectors)
+    return np.dot(data - np.mean(data, axis=0), eigenvectors.T)
 
 
 def process_dataset(dir_path, categories, n_components=10, calculate_covariance=True):
@@ -77,19 +84,19 @@ def process_dataset(dir_path, categories, n_components=10, calculate_covariance=
 
     # load images
     for category in categories:
-        image_paths, selected_files = get_image_paths(dir_path, category)
-        selected_files_info[category] = selected_files
+        image_paths = get_image_paths(dir_path, category)  
+        selected_files_info[category] = [os.path.basename(path) for path in image_paths]
         category_images = load_images(image_paths)
         all_images.append(category_images)
 
-    all_images = np.vstack(all_images)
+    all_images = np.vstack(all_images) 
 
-    #  calc cov matrix 
+    #  calc cov matrix
     if calculate_covariance:
         covariance_matrix = calculate_and_save_covariance(
             all_images, f"{dir_path}_covariance_matrix.txt"
         )
-        # calc  eigenvalues & eigenvectors 
+        # calc  eigenvalues & eigenvectors
         eigenvalues, eigenvectors = calculate_eigenvalues_eigenvectors(
             covariance_matrix
         )
@@ -100,21 +107,25 @@ def process_dataset(dir_path, categories, n_components=10, calculate_covariance=
         eigenvalues = pca.explained_variance_
         eigenvectors = pca.components_
 
-    # top 50 eingenvalues 
+    # top 50 eingenvalues
     top_50_eigenvalues(eigenvalues)
 
-    # sace eingen faces 
+    # top eingen faces
     top_10_eigenfaces(eigenvectors, prefix=f"{dir_path}_eigenface")
 
-  # Trace and save images to a new vector space
+    # Trace and save images to a new vector space
     projected_data = project_to_new_space(all_images, eigenvectors)
     np.savetxt(f"{dir_path}_projected_data.txt", projected_data)
 
     for category, files in selected_files_info.items():
         print(f"Selected files for {category}: {files}")
 
+
 # Example usage
 dir_path = "."  # Update with your actual images path
-categories = ["happy", "sad"] # images dir name 
+categories = ["happy", "sad"]  # images dir name
+categories_2 = ["mutlu","uzgun"]
+process_dataset(
+    dir_path, categories, 10, calculate_covariance=False)  # To use PCA, set it to false
 
-process_dataset(dir_path, categories, 10, calculate_covariance=False) # To use PCA, set it to false
+process_dataset(dir_path, categories_2, 20, calculate_covariance=True)
